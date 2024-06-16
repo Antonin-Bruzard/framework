ceb2518766c7febbab358e11abefce0f0c245981
antonin.bruzard <antonin.bruzard@gmail.com>
2024-06-17 01:34:18 +0200

    WIP : initializing the app and adding autoload

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
 <?php
 
-require_once('src/app.php');
-require_once('src/kernel.php');
+use App\Controller\MainController;
+use Src\App;
+
+require_once 'src/functions.php';
+require 'vendor/autoload.php';
+
+App::get();
+
+$test = new MainController();
+
+
+// autoload des class -> intégrer en récursif les namespace
+
+// TODO: Utiliser les annotation php pour faire le système de routing
+
+// routing
+
+
 
 // La en gros, on va chercher a démarrer notre app (UNE SEULE FOIS):
 // - conf
@@ -9,5 +25,5 @@ require_once('src/kernel.php');
 // conteneur d'injection d déoendance (DIC/DI).
 // Router ...
 
-dp($_SERVER);
+//dp($_SERVER);
 
